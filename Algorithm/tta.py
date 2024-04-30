46f2e5513056f021c839423e9b2b6c88a4fc8993
Qian Zeng <142384657+Casperqian@users.noreply.github.com>
2024-04-30 21:32:55 +0800

    Update tta.py

--- Algorithm/tta.py
+++ Algorithm/tta.py
@@ -136,95 +136,14 @@ class Algorithm(torch.nn.Module):
     def predict(self, x):
         raise NotImplementedError
 
-
-class T3A(Algorithm):
-    """
-    Test Time Template Adjustments (T3A)
-    """
-
-    def __init__(self, featurizer, classifier, args, filter_K = 100, num_classes = 5):
-        super(T3A, self).__init__(args)
-        self.featurizer = featurizer
-        self.classifier = classifier
-
-        warmup_supports = self.classifier.weight.data
-        self.warmup_supports = warmup_supports
-        warmup_prob = self.classifier(self.warmup_supports)
-        self.warmup_ent = softmax_entropy(warmup_prob)
-        self.warmup_labels = torch.nn.functional.one_hot(warmup_prob.argmax(1), num_classes=num_classes).float()
-
-        self.supports = self.warmup_supports.data
-        self.labels = self.warmup_labels.data
-        self.ent = self.warmup_ent.data
-
-        self.filter_K = filter_K
-        self.num_classes = num_classes
-        self.softmax = torch.nn.Softmax(-1)
-
-    def forward(self, x, adapt=False):
-        z = x
-        torch.cuda.empty_cache()
-        if adapt:
-            # online adaptation
-            p = self.classifier(z)
-            z = z
-            yhat = torch.nn.functional.one_hot(p.argmax(1), num_classes=self.num_classes).float()
-            ent = softmax_entropy(p)
-
-            # prediction
-            self.supports = self.supports.to(z.device)
-            self.labels = self.labels.to(z.device)
-            self.ent = self.ent.to(z.device)
-            self.supports = torch.concat([self.supports, z])
-            self.labels = torch.concat([self.labels, yhat])
-            self.ent = torch.concat([self.ent, ent])
-
-        supports, labels = self.select_supports()
-        supports = torch.nn.functional.normalize(supports, dim=1)
-        weights = (supports.T @ (labels))
-        z = z.to(weights.device)
-        return z @ torch.nn.functional.normalize(weights, dim=0)
-
-    def select_supports(self):
-        ent_s = self.ent
-        y_hat = self.labels.argmax(dim=1).long()
-        filter_K = self.filter_K
-        if filter_K == -1:
-            indices = torch.LongTensor(list(range(len(ent_s))))
-
-        indices = []
-        indices1 = torch.LongTensor(list(range(len(ent_s)))).to(ent_s.device)
-        # print('indices1',indices1.shape)
-        for i in range(self.num_classes):
-            _, indices2 = torch.sort(ent_s[y_hat == i])
-            indices.append(indices1[y_hat == i][indices2][:filter_K])
-            # print('for',indices1[y_hat == i][indices2][:filter_K].shape)
-        indices = torch.cat(indices)
-        self.supports = self.supports[indices]
-        self.labels = self.labels[indices]
-        self.ent = self.ent[indices]
-
-        return self.supports, self.labels
-
-    def predict(self, x, adapt=False):
-        return self(x, adapt)
-
-    def reset(self):
-        self.supports = self.warmup_supports.data
-        self.labels = self.warmup_labels.data
-        self.ent = self.warmup_ent.data
-
-
-class FAN(Algorithm):
+class TTFA(Algorithm):
+    '''Test-Time Classifier Adjustment Module for Model-Agnostic Domain Generalization & Test-Time Adaptation via Self-Training with Nearest Neighbor Information'''
     def __init__(self, featurizer, classifier, n_outputs, args, filter_K = 100, steps = 3, num_ensemble = 5, lr = 1e-3,
                  k = 8, tau = 10,  num_classes = 5):
         super(FAN, self).__init__(args)
         self.featurizer = featurizer
         self.classifier = classifier
         self.n_outputs = n_outputs
-
-
-        # store supports and corresponding labels
 
         warmup_supports = self.classifier.weight.data
         self.warmup_supports = warmup_supports
@@ -236,7 +155,6 @@ class FAN(Algorithm):
         self.supports = self.warmup_supports.data
         self.labels = self.warmup_labels.data
 
-        # hparams
         self.filter_K = filter_K
         self.steps = steps
         self.num_ensemble = num_ensemble
@@ -245,9 +163,6 @@ class FAN(Algorithm):
         self.tau = tau
         self.init_mode = 'kaiming_normal'
         self.num_classes = num_classes
-
-
-        # multiple projection heads and its optimizer
         self.mlps = BatchEnsemble(self.n_outputs, self.n_outputs // 4, self.num_ensemble,
                                   self.init_mode).cuda()
         self.optimizer = torch.optim.Adam(self.mlps.parameters(), lr=self.lr, eps=1e-8)
